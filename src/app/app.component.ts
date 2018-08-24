e80bcdf3f2212705ee579ae0a6788549d7cb87b9
zxl <xiaolg_zhou@163.com>
2018-08-24 17:28:50 +0800

    更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { FileAddPage } from '../pages/file-add/file-add';
 import { QuestiondetailsPage } from '../pages/questiondetails/questiondetails';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ReleaseinfoPage } from '../pages/releaseinfo/releaseinfo';
 
 @Component({
   templateUrl: 'app.html'
