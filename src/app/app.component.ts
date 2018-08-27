f50e5b32e2fa94bf20c89c16d00752ba82cbc9b0
李明 <672486796@qq.com>
2018-08-27 15:28:21 +0800

    asdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { IntegralPage } from '../pages/integral/integral';
 import { TabsPage } from '../pages/tabs/tabs';
-import { InstructionsPage } from '../pages/instructions/instructions';
-import { MeanswerPage } from '../pages/meanswer/meanswer';
-import { MycollectionPage } from '../pages/mycollection/mycollection';
 
 @Component({
   templateUrl: 'app.html'
