5fb061db12fabb474344df649ff94c302f8da32b
李明 <672486796@qq.com>
2018-08-29 13:20:45 +0800

    ad

--- src/pages/set/set.ts
+++ src/pages/set/set.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HttpProvider } from '../../providers/http/http';
+import { LoginPage } from '../login/login';
 
 /**
  * 系统设置
@@ -11,11 +13,19 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SetPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public httpServe: HttpProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SetPage');
   }
 
+  /**
+   * 退出账号
+   */
+  logout(){
+    this.httpServe.confim('确定退出当前账号？',(res)=>{
+      this.navCtrl.setRoot(LoginPage);
+    });
+  }
 }
