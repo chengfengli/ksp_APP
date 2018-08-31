b3fa4f2792891e3b55c0a6c70d9455e6d0f131c7
李明 <672486796@qq.com>
2018-08-31 10:02:02 +0800

    ad

--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -13,7 +13,7 @@ import { Storage } from '@ionic/storage';
 */
 @Injectable()
 export class HttpProvider {
-  public apiURL = 'http://192.168.1.85:7004/app';
+  public apiURL = 'http://192.168.0.126:7004/app';
 
   constructor(private http: Http,public toastCtrl:ToastController,private alertCtrl: AlertController, private loadingCtrl: LoadingController,public storage: Storage,private network: Network) {}
 
@@ -58,7 +58,6 @@ export class HttpProvider {
     this.headers = new Headers({'Content-Type':'application/json'});
     this.storageGet('token').subscribe((res)=>{
       if(!this.isEmpty(res)){
-        this.headers.append('Authorization','sfasdf');
         option.params.token = res;
       }
       this.http.get(url+this.encode(option.params),{headers :this.headers}).map(res=>res.json()) //返回数据转换成json
@@ -91,7 +90,6 @@ export class HttpProvider {
     this.headers = new Headers({'Content-Type':'application/json'});
     this.storageGet('token').subscribe((res)=>{
       if(!this.isEmpty(res)){
-        this.headers.append('Authorization',res);
         option.url+='?token='+res;
       }
       this.http.post(this.apiURL+option.url,JSON.stringify(option.params),{headers :this.headers}).map(res=>res.json()) //返回数据转换成json
